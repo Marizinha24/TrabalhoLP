11cd6d77abbeae545d7fd695f289b1bd8ef2da62
MariG <marigoncalves200624@gmail.com>
2026-05-24 19:07:59 -0300

    Realizado a criação de banco, DAO e classes

---
+++ Properties/DAO/ConnectionSQL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace TrabalhoLP.Properties.DAO
+{
+    public class ConnectionSQL
+    {
+        private string connectionString = @"Data Source=localhost\SQLEXPRESS;Initial Catalog=PetCare;Integrated Security=True;";
+
+        public SqlCommand MountQuery(string sql)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            try
+            {
+                connection.Open();
+                return command;
+            }
+            catch (Exception ex)
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+                throw new Exception("Erro ao conectar ao banco: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Properties/DAO/UsuarioDAO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.SqlClient;
+using TrabalhoLP.Properties.Model;
+
+namespace TrabalhoLP.Properties.DAO
+{
+    public class UsuarioDAO
+    {
+        private ConnectionSQL db = new ConnectionSQL();
+
+        public void Inserir(Usuario usuario)
+        {
+            string sql = $"INSERT INTO Usuarios (Nome, CPF, Telefone, Email, Endereco) VALUES ('{usuario.Nome}', '{usuario.CPF}', '{usuario.Telefone}', '{usuario.Email}', '{usuario.Endereco}')";
+            db.MountQuery(sql);
+        }
+    }
+}
